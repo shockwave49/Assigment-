710e5574d3e63009b30ea58c68d29819d7be6c01
AYUSH UNIYAL <155154602+shockwave49@users.noreply.github.com>
2024-12-18 22:36:06 +0530

    Delete src directory

--- src/main/java/com/blog/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blog.entities.User;
-
-public interface UserRepo extends JpaRepository<User , Integer> {
-
-}
